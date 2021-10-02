bd1b51aa6fbdddb45345b1e05bb92db29e7fef7b
Eugene Kuznietsov <ekuznietsov@deloitte.com>
2021-10-03 00:02:36 +0300

    wip

--- shootingtrack/lib/main.dart
+++ shootingtrack/lib/main.dart
@@ -4,9 +4,22 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:shootingtrack/app/shooting_track_app.dart';
 import 'package:shootingtrack/di/di.dart';
 
+import 'data/entities/entities.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
+
+  await initHive();
+
   registerDependencies();
   runApp(const ShootingTrackApp());
+}
+
+Future<void> initHive() async {
+  await Hive.initFlutter();
+  Hive
+    ..registerAdapter(WeaponAdapter())
+    ..registerAdapter(ManufacturerAdapter())
+    ..registerAdapter(ModelAdapter())
+    ..registerAdapter(GaugeAdapter());
 }
